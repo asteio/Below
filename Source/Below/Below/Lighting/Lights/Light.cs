48972277aa63d9b157261165e6aa27ef2b727c80
Unknown <asteio2000@gmail.com>
2018-09-19 15:31:59 -0400

    comment edits

--- Source/Below/Below/Lighting/Lights/Light.cs
+++ Source/Below/Below/Lighting/Lights/Light.cs
@@ -1,6 +1,4 @@
-﻿//All code and graphical content is 100% originally created by the Shark Boys group
-//aside from the methods and classes used directly from the Monogame/XNA Framework
-
+﻿
 using Microsoft.Xna.Framework;
 
 namespace Below
--- Source/Below/Below/Lighting/Lights/PointLight.cs
+++ Source/Below/Below/Lighting/Lights/PointLight.cs
@@ -1,6 +1,4 @@
-﻿//All code and graphical content is 100% originally created by the Shark Boys group
-//aside from the methods and classes used directly from the Monogame/XNA Framework
-
+﻿
 namespace Below
 {
 	public class PointLight : Light
